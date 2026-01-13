6f567bfad014a4c3b8e424b11f9058d3c7582515
Rohith <rohithvh973@gmail.com>
2026-01-13 11:19:25 +0530

    python 1st session

--- tc001.py
+++ tc001.py
@@ -12,3 +12,13 @@ b= 20
 print("addition",a+b)
 
 print("subtraction",a-b)
+
+a= 100
+b= 20
+print("addition",a+b)
+
+print("subtraction",a-b)
+print("division",a/b)
+print("multiplication",a*b)
+print("modulas",a%b)
+print("floor division",a//b)
